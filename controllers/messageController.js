2a5e0f0117b03cd24c2abac07c70df64fa1c6858
Ming <ben014335@gmail.com>
2023-10-24 20:13:04 +0800

    feat：建立AWS相關服務  因應新需求做調整： 1.運用AWS S3儲存圖片 2.運用AWS CloudFront建立CDN系統 3.運用AWS RDS建立資料庫 調整項目：  modified:   app.js 1.controllers/messageController.js 2.models/messageModel.js 3.package-lock.json 4.package.json 5.views/index.ejs 6..env.sample 7.fileService.js 8.uploads/

--- controllers/messageController.js
+++ controllers/messageController.js
@@ -1,20 +1,55 @@
 // controllers/messageController.js
+
 const messageModel = require("../models/messageModel");
+const fileService = require("../fileService");
+const fs = require("fs");
 
 module.exports = {
+  // 處理獲取所有訊息的請求
   getAllMessages: (req, res) => {
-    const messages = messageModel.getAllMessages();
-    //渲染views資料夾內的index.ejs
-    res.render("index", { messages });
+    messageModel.getAllMessages((err, messages) => {
+      if (err) {
+        console.error("從資料庫獲取訊息時出現錯誤: " + err);
+        res.status(500).send("內部伺服器錯誤");
+      } else {
+        res.render("index", { messages });
+      }
+    });
   },
+
+  // 處理新增訊息的請求
   addMessage: (req, res) => {
-    //取得表單留言
     const message = req.body.message;
-    //如果有圖片，就轉成圖片位置連結，沒有的話就是null
-    const imageLink = req.file ? `/uploads/${req.file.filename}` : null;
+    const image = req.file;
 
-    messageModel.addMessage(message, imageLink);
-
-    res.redirect("/");
+    if (image) {
+      // 如果有上傳的圖片
+      fileService.uploadFile(image, (fileErr, imageLink) => {
+        if (fileErr) {
+          console.error("上傳檔案時出現錯誤: " + fileErr);
+          res.status(500).send("內部伺服器錯誤");
+        } else {
+          messageModel.addMessage(message, imageLink, (dbErr) => {
+            if (dbErr) {
+              console.error("將訊息存儲到資料庫時出現錯誤: " + dbErr);
+              res.status(500).send("內部伺服器錯誤");
+            } else {
+              fs.unlinkSync(image.path);
+              res.redirect("/");
+            }
+          });
+        }
+      });
+    } else {
+      // 處理未上傳圖片的情況
+      messageModel.addMessage(message, null, (dbErr) => {
+        if (dbErr) {
+          console.error("將訊息存儲到資料庫時出現錯誤: " + dbErr);
+          res.status(500).send("內部伺服器錯誤");
+        } else {
+          res.redirect("/");
+        }
+      });
+    }
   },
 };
--- models/messageModel.js
+++ models/messageModel.js
@@ -1,14 +1,42 @@
 // models/messageModel.js
-const messages = [];
+const mysql = require("mysql2");
+
+// 創建 MySQL 連接池
+const pool = mysql.createPool({
+  host: process.env.RDS_HOSTNAME,
+  user: process.env.RDS_USERNAME,
+  password: process.env.RDS_PASSWORD,
+  database: process.env.RDS_DATABASE,
+});
+
+// 從連接池中創建 promise 連接
+const db = pool.promise();
 
 module.exports = {
-  getAllMessages: () => messages,
-  addMessage: (message, imageLink) => {
-    // 將留言與圖片合為同一個留言顯示
-    const newMessage = {
-      text: message,
-      image: imageLink,
-    };
-    messages.push(newMessage);
+  // 非同步函數，用於檢索所有留言
+  getAllMessages: async (callback) => {
+    try {
+      // 使用 promise 連接執行 SQL 查詢，獲得結果和字段
+      const [results, fields] = await db.query("SELECT * FROM messages");
+      //console.log("從資料庫檢索到的留言:", results);
+      callback(null, results);
+    } catch (err) {
+      console.error("從資料庫查詢留言時出現錯誤: " + err);
+      callback(err, null);
+    }
+  },
+
+  // 非同步函數，用於新增留言和圖片連結
+  addMessage: async (message, imageLink, callback) => {
+    // 定義 SQL 查詢語句
+    const sql = "INSERT INTO messages (message, imageLink) VALUES (?, ?)";
+    try {
+      // 使用 promise 連接執行 SQL 查詢，傳入留言內容和圖片連結
+      await db.query(sql, [message, imageLink]);
+      callback(null);
+    } catch (err) {
+      console.error("在資料庫中存儲留言時出現錯誤: " + err);
+      callback(err);
+    }
   },
 };
